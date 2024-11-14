e82e93e9b14f3f96a7051b13745ae439c10f5353
Xavier Leclercq <xavier@leclercq.dev>
2024-11-14 21:03:41 +0000

    Start adding red-black tree (#18)

--- include/Ishiko/DataStructures/RedBlackTree.hpp
+++ include/Ishiko/DataStructures/RedBlackTree.hpp
@@ -6,9 +6,42 @@
 
 namespace Ishiko
 {
+    template<class ValueType>
     class RedBlackTree
     {
+    public:
+        class Node
+        {
+        };
+
+        RedBlackTree() noexcept = default;
+        ~RedBlackTree() noexcept;
+
+        void insert(const ValueType& value);
+
+        size_t size() const noexcept;
+
+    private:
+        size_t m_size = 0;
+        Node* m_root = nullptr;
     };
+}
+
+template<class ValueType>
+Ishiko::RedBlackTree<ValueType>::~RedBlackTree() noexcept
+{
+    delete m_root;
+}
+
+template<class ValueType>
+void Ishiko::RedBlackTree<ValueType>::insert(const ValueType& value)
+{
+}
+
+template<class ValueType>
+size_t Ishiko::RedBlackTree<ValueType>::size() const noexcept
+{
+    return m_size;
 }
 
 #endif
