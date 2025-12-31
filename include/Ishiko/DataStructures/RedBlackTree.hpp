c6c70c3aeca848bb2782c93fdd2226f0c8bef8fc
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2025-12-31 19:56:08 +0000

    Add missing header include

--- include/Ishiko/DataStructures/RedBlackTree.hpp
+++ include/Ishiko/DataStructures/RedBlackTree.hpp
@@ -1,8 +1,10 @@
-// SPDX-FileCopyrightText: 2005-2024 Xavier Leclercq
+// SPDX-FileCopyrightText: 2005-2025 Xavier Leclercq
 // SPDX-License-Identifier: BSL-1.0
 
 #ifndef GUARD_ISHIKO_CPP_DATASTRUCTURES_REDBLACKTREE_HPP
 #define GUARD_ISHIKO_CPP_DATASTRUCTURES_REDBLACKTREE_HPP
+
+#include <cstddef>
 
 namespace Ishiko
 {
